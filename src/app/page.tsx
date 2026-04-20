de3062b6ed8f76b12963e681cdbbc09c10600446
Czeizing Hedvig <czeizinghedi@gmail.com>
2026-04-20 13:06:08 +0200

    fix: correct destructuring syntax in baseLayer state initialization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
-import MapWrapper from '@components/MapWrapper';
 import AppBar from '@mui/material/AppBar';
 import Container from '@mui/material/Container';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
+import MapWrapper from '@components/MapWrapper';
 
 export default function HomePage() {
 	return (
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -21,7 +21,7 @@ const drawerWidth = 240;
 
 const Sidebar = ({mapInstance}: {mapInstance: RefObject<Map | null>} ) => {
     
-	const [baseLayer, setBaseLayer] = useState(Object.entries(BaseLayersConfig).find(([_, layer]) => layer.getVisible())?.[0] || 'osm-default');
+	const [baseLayer, setBaseLayer] = useState(Object.entries(BaseLayersConfig).find(([, layer]) => layer.getVisible())?.[0] || 'osm-default');
     const [visibleOverlays, setVisibleOverlays] = useState<
 		Record<string, boolean>
 	>(
